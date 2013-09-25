8c32fed0875e92470b5dec2faa0f35aaf42b48ce
Natasha Kerensikova <natacha@instinctive.eu>
2013-09-25 20:07:05 +0000

    coverage.sh: clear counters before each run

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 gnatmake -p -Ptests -XMODE=Coverage || exit $?
+lcov --directory coverage/obj --zerocounters
 coverage/bin/test_all
 lcov --gcov-tool gnatgcov --directory coverage/obj --output coverage/test-info.dat --capture || exit $?
 genhtml --output-dir coverage coverage/test-info.dat
