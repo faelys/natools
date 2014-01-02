2c5c3fb018f93afb895745d9bb5afbf651fb1b0f
Natasha Kerensikova <natacha@instinctive.eu>
2014-01-02 19:22:57 +0000

    coverage.sh: update the script for gcc-aux

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-gnatmake -p -Ptests -XMODE=Coverage || exit $?
+
+: ${GNATPATH:=/usr/local/gcc-aux/bin}
+
+${GNATPATH}/gnatmake -p -Ptests -XMODE=Coverage || exit $?
 lcov --directory coverage/obj --zerocounters
 coverage/bin/test_all >coverage.log
 tail -n 4 coverage.log
-lcov --gcov-tool gnatgcov --directory coverage/obj --output coverage/test-info.dat --capture || exit $?
+lcov --gcov-tool ${GNATPATH}/gcov --directory coverage/obj --output coverage/test-info.dat --capture || exit $?
 genhtml --output-dir coverage coverage/test-info.dat
