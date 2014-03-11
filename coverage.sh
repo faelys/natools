a65390671b5298c6d8b98722d122ce55a71f2b73
Natasha Kerensikova <natacha@instinctive.eu>
2014-03-11 19:33:20 +0000

    coverage.sh: add more configuration variables for better flexibility

--- coverage.sh
+++ coverage.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 : ${GNATPATH:=/usr/local/gcc-aux/bin}
+: ${LCOV_DATA:=coverage/test-info.dat}
+: ${LCOV_HTML_DIR:=coverage}
+: ${TEST_LOG:=coverage.log}
 
 ${GNATPATH}/gnatmake -p -Ptests -XMODE=Coverage || exit $?
 lcov --directory coverage/obj --zerocounters
-coverage/bin/test_all >coverage.log
-tail -n 4 coverage.log
-lcov --gcov-tool ${GNATPATH}/gcov --directory coverage/obj --output coverage/test-info.dat --capture || exit $?
-genhtml --output-dir coverage coverage/test-info.dat
+coverage/bin/test_all > "${TEST_LOG}"
+tail -n 4  "${TEST_LOG}"
+lcov --gcov-tool ${GNATPATH}/gcov --directory coverage/obj --output "${LCOV_DATA}" --capture || exit $?
+genhtml --output-dir "${LCOV_HTML_DIR}" "${LCOV_DATA}"
